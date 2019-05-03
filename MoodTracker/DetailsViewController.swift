dd12c54e3f63157afac874e25efda66673cd72ad
Caroline Lubbe <clubbe9234@stu.d214.org>
2019-05-03 11:21:13 -0500

    added IBActions for all buttons, created a segue, and embedded in a nav controller

--- MoodTracker/DetailsViewController.swift
+++ MoodTracker/DetailsViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    var mood: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- MoodTracker/MoodChoiceViewController.swift
+++ MoodTracker/MoodChoiceViewController.swift
@@ -9,15 +9,59 @@
 import UIKit
 import EventKit
 
-var mood: String!
-
 class MoodChoiceViewController: UIViewController {
+    
+    var mood: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenRedButtonPressed(_ sender: Any) {
+        mood = "Angry"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenOrangeButtonPressed(_ sender: Any) {
+        mood = "Excited"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenYellowButtonPressed(_ sender: Any) {
+        mood = "Happy"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenGreenButtonPressed(_ sender: Any) {
+        mood = "Jealous"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenBlueButtonPressed(_ sender: Any) {
+        mood = "Sad"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenPurpleButtonPressed(_ sender: Any) {
+        mood = "Productive"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenPinkButtonPressed(_ sender: Any) {
+        mood = "Calm"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    @IBAction func whenBlackButtonPressed(_ sender: Any) {
+        mood = "Dead"
+        performSegue(withIdentifier: "moodSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! DetailsViewController
+        dvc.mood = mood
+    }
+    
 }
 
