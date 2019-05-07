2dc2e4802aeffb8f9b64d54c4f05fdad828d02f6
Caroline Lubbe <clubbe9234@stu.d214.org>
2019-05-07 11:12:44 -0500

    slightly modified the colors of the buttons and added the colors I used in the code

--- MoodTracker/MoodChoiceViewController.swift
+++ MoodTracker/MoodChoiceViewController.swift
@@ -21,41 +21,49 @@ class MoodChoiceViewController: UIViewController {
     @IBAction func whenRedButtonPressed(_ sender: Any) {
         mood = "Angry"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 255, G: 0, B: 0
     }
     
     @IBAction func whenOrangeButtonPressed(_ sender: Any) {
         mood = "Excited"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 255, G: 142, B: 0
     }
     
     @IBAction func whenYellowButtonPressed(_ sender: Any) {
         mood = "Happy"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 255, G: 255, B: 0
     }
     
     @IBAction func whenGreenButtonPressed(_ sender: Any) {
         mood = "Jealous"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 0, G: 165, B: 9
     }
     
     @IBAction func whenBlueButtonPressed(_ sender: Any) {
         mood = "Sad"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 0, G: 45, B: 255
     }
     
     @IBAction func whenPurpleButtonPressed(_ sender: Any) {
         mood = "Productive"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 169, G: 0, B: 223
     }
     
     @IBAction func whenPinkButtonPressed(_ sender: Any) {
         mood = "Calm"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is R: 255, G: 64, B: 192
     }
     
-    @IBAction func whenBlackButtonPressed(_ sender: Any) {
+    @IBAction func whenGreyButtonPressed(_ sender: Any) {
         mood = "Dead"
         performSegue(withIdentifier: "moodSegue", sender: nil)
+        // button color is 50% grayscale slider
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
